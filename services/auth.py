e35fa9a27e816c4f49cec5677b69c774e8d71981
adjanour <adjanour@icloud.com>
2025-04-02 07:57:00 +0000

    fabc

--- services/auth.py
+++ services/auth.py
@@ -1,7 +1,6 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 import jwt
-from jwt import PyJWTError
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
@@ -40,6 +39,6 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         email: str = payload.get("sub")
         if email is None:
             raise credentials_exception
-    except PyJWTError:
+    except :
         raise credentials_exception
     return {"email": email}
